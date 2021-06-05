9a550fccee932a6f42685bd10228281aa3349f14
Luiz Almeida <luizorst@hotmail.com>
2021-06-05 19:06:56 -0300

    [FIX] clean workingOn file

--- workingOn.cpp
+++ workingOn.cpp
@@ -1,27 +1,11 @@
 #include <bits/stdc++.h>
-#define _                         \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define pb push_back
 
-int main() {
-    int numberOfTests = 0;
-    int lines = 0, columns = 0;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
-    std::cin >> numberOfTests;
+int main(){
 
-    char black = 'B', white = 'W';
-
-    for (int i = 0; i < numberOfTests; ++i) {
-        std::cin >> lines >> columns;
-        for (int inside_i = 0; inside_i < lines - 1; ++inside_i) {
-            for (int inside_j = 0; inside_j < columns; inside_j++) {
-                std::cout << black;
-            }
-            std::cout << std::endl;
-        }
-        for (int lastLine = 0; lastLine < columns - 1; lastLine++) {
-            std::cout << black;
-        }
-        std::cout << white << std::endl;
-    }
 }
